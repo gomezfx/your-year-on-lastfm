2eac74357ac6595e713258e1e56a6e3cfed046d1
Aldo Gomez <gomezfx@gmail.com>
2015-02-26 00:15:01 -0500

    Added some animations and top album parsing

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 var AlbumComponent = Vue.extend({
   template:
     '<div class="container">' +
-      '<div class="half-container">' +
+      '<div class="album-container">' +
         '<div class="vertical-center">' +
-          '<span class="album-number">1</span><br><br><br><br><br><br><br><br>' +
-          '<span class="album-name">{{ album.name }}</span><br><br>' +
-          '<span class="subtext">by</span><br><br>' +
-          '<span class="album-artist">{{ album.artist }}</span>' +
+          '<div class="album-info">' +
+            '<span class="album-number">{{ $index + 1}}</span><br><br>' +
+            '<span class="album-name">{{ album.name }}</span><br><br>' +
+            '<span class="subtext">by</span><br><br>' +
+            '<span class="album-artist">{{ album.artist }}</span>' +
+          '</div>' +
         '</div>' +
       '</div>' +
-      '<div class="half-container">' +
-        '<img class="album vertical-center" src="sample.jpg"></img>' +
+      '<div class="album-container">' +
+        '<div class="vertical-center">' +
+          '<div class="album-art"></div>' +
+        '</div>' +
       '</div>' +
     '</div>'
 });
@@ -21,6 +25,7 @@ var main = new Vue({
   el: '#main',
 
   data: {
+    initialized: false,
     username: '',
     avatarSource: '',
     albums: [],
@@ -40,7 +45,12 @@ var main = new Vue({
       var self = this;
       client.getInfo('gomezfx').then(function(user) {
         self.username = user.name;
-        self.avatarSource = user.avatarSource;
+        var img = new Image();
+        img.onload = function() {
+         self.avatarSource = user.avatarSource;
+         self.initialized = true;
+        };
+        img.src = user.avatarSource;
 
         return client.getWeeklyChartList('gomezfx');
       }).then(function(dates) {
@@ -54,7 +64,30 @@ var main = new Vue({
         
         return Q.all(results);
       }).done(function (albums) {
-        console.log(albums.length);
+        var albumMap = {};
+        for (var i = 0; i < albums.length; i++) {
+          var chart = albums[i];
+          for (var j = 0; j < chart.length; j++) {
+            var albumItr = chart[j];
+            if(albumMap.hasOwnProperty(albumItr.name)) {
+              albumMap[albumItr.name].playCount += albumItr.playCount;
+            } else {
+              albumMap[albumItr.name] = albumItr;
+            }
+          }
+        }
+        var albumArray = new Array();
+        for (var key in albumMap) {
+         var obj = albumMap[key];
+         albumArray.push(obj);
+        }
+        albumArray.sort(function (a, b) {
+          return b.playCount - a.playCount;
+        });
+
+        for(var i = 0; i < 5; i++) {
+          self.albums.push(albumArray[i]);
+        }        
       });
     }
   }
